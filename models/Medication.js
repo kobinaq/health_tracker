f51e042f010818e4b0b92c8425516b753217da08
kobinaq <125668528+kobinaq@users.noreply.github.com>
2025-01-25 10:07:04 +0000

    Added Mongoose models for Appointment and Medication

---
+++ models/Medication.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const MedicationSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  description: String,
+  severity: { type: Number, min: 1, max: 10 },
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Medication', MedicationSchema);
